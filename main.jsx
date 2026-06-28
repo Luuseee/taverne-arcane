d4bb2c0b9acb0e12cba5f2bfd7d494493cd0a363
Luuseee <nakygamee@gmail.com>
2026-06-28 13:57:54 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,1 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
+// On s'assure que React est disponible globalement si un composant le cherche
+window.React = React;
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
